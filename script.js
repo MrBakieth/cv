cf72551dd80e9ae25558ca36a591d3f0766c9cfd
Mr. Furkan <126611917+MrBakieth@users.noreply.github.com>
2023-08-15 17:19:45 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
-      // 3 saniye sonra farklı bir sayfaya yönlendirme
       window.location.href = "welcome.html";
-    }, 3000); // 3 saniye
+    }, 3000);
+    
   });
